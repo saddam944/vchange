b9282b051331b722c69f685f72dc5da5ee6f2310
Saddam Hossain <saddam944@gmail.com>
2017-08-28 13:44:35 +0600

    Update create-files-for-timeslots.py

--- create-files-for-timeslots.py
+++ create-files-for-timeslots.py
@@ -1,8 +1,4 @@
 
-# coding: utf-8
-
-# In[29]:
-
 import csv
 import pandas as pd
 
@@ -34,9 +30,5 @@ for idx in range(len(pair_slots)):
 for tm in range(len(final_slots)):
     csvdataframe=pd.DataFrame(final_slots[tm],columns=['date', 'tweets'])
     csvdataframe.to_csv("E:/PS4AusGamer_int"+str(tm)+".csv",index=False)
-
-
-# In[ ]:
-
 
 
